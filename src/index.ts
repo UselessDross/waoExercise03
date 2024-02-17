9959ce9bc16f8475663da5220571ffdd98191774
Useless-4every1 <104831784+UselessDross@users.noreply.github.com>
2024-02-17 17:50:20 +0100

    added dockerfill; added ignore files; ran docker build -t wao-image:1.0 .

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ import { router } from "./router-controlls";
 const app = express()
 import { seed } from './seed';
 
-const port = 3000;
+const port = process.env.PORT || 8080;
 app.use('/orders', router)
 app.get('/seed', seed)
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`)
   })
    
